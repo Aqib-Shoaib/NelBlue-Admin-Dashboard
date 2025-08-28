e9d1a9f38f399d9fbe8792ad4cf8e80601d0cf6e
Aqib Shoaib <bluepitviper1233@gmail.com>
2025-08-29 01:58:57 +0500

    burger icon

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -23,9 +23,7 @@ function Layout() {
               className='inline-flex items-center justify-center w-10 h-10 rounded-md border hover:bg-gray-50 active:scale-95 transition'
             >
               {/* Hamburger icon */}
-              <span className='block w-5 h-0.5 bg-black mb-1.5'></span>
-              <span className='block w-5 h-0.5 bg-black mb-1.5'></span>
-              <span className='block w-5 h-0.5 bg-black'></span>
+              <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15"><path fill="currentColor" fill-rule="evenodd" d="M1.5 3a.5.5 0 0 0 0 1h12a.5.5 0 0 0 0-1zM1 7.5a.5.5 0 0 1 .5-.5h12a.5.5 0 0 1 0 1h-12a.5.5 0 0 1-.5-.5m0 4a.5.5 0 0 1 .5-.5h12a.5.5 0 0 1 0 1h-12a.5.5 0 0 1-.5-.5" clip-rule="evenodd"/></svg>
             </button>
             <span className='font-bold text-[#023AA2]'>NELAUTOS</span>
           </div>
